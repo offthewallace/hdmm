e6d8519bca97b25a1effea943a1dbfa0f09b8a01
offthewallace <gbh5146@psu.edu>
2023-04-13 15:59:40 -0400

    run more exp

--- experiments/journal/compute_strategies.py
+++ experiments/journal/compute_strategies.py
@@ -5,6 +5,8 @@ import numpy as np
 import benchmarks
 import pickle
 import os
+import time
+
 
 def get_domain(W):
     if isinstance(W, workload.VStack):
@@ -23,7 +25,7 @@ def default_params():
     params['approx'] = 'True'
     params['dataset'] = 'adult'
     params['workload'] = 1
-    params['output'] = 'hderror_2023_04_10.csv'
+    params['output'] = 'hderror_2023_04_12.csv'
 
     return params
 
@@ -47,18 +49,22 @@ if __name__ == '__main__':
     #temp1 = templates.DefaultKron(ns, approx)
     #temp2 = templates.DefaultUnionKron(ns, len(W.matrices), approx)
     temp3 = templates.Marginals(ns, approx)
+    t0 = time.time()
 
     #loss1 = temp1.optimize(W)
     #loss2 = temp2.optimize(W)
     loss3 = temp3.optimize(W)
+    t1 = time.time()
 
     losses = {}
     #losses['kron'] = np.sqrt(loss1 / W.shape[0])
     #losses['union'] = np.sqrt(loss2 / W.shape[0])
     losses['marg'] = np.sqrt(loss3 / W.shape[0])
+    line = ' %.6f, %.6f' % (t1-t0,losses['marg'])
+    print(line)
 
     if args.output is not None:
         with open(args.output, 'a') as f:
             for param in losses.keys():
-                key = (args.dataset, args.workload, approx, param, losses[param])
-                f.write('%s, %d, %s, %s, %.4f \n' % key)
+                key = (args.dataset, args.workload, approx, param, losses[param],t1-t0)
+                f.write('%s, %d, %s, %s, %.4f,%.6f\n' % key)
