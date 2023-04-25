6f27d8682e692beb85537b19dec9d48d8dd182c5
offthewallace <gbh5146@psu.edu>
2023-04-25 15:00:00 -0400

    add appox diag calculation

--- src/hdmm/error.py
+++ src/hdmm/error.py
@@ -40,7 +40,7 @@ def expected_error(W, A, eps=np.sqrt(2), delta=0):
     else:
         trace = X.trace()
     var = 2.0 / eps**2
-    return var * delta**2 * trace
+    return trace
 
 def rootmse(W, A, eps=np.sqrt(2), delta=0):
     """ compute a normalized version of expected squared error """
@@ -74,6 +74,23 @@ def per_query_error(W, A, eps=np.sqrt(2), delta=0, normalize=False):
     err = X.diag()
     answer = var * delta**2 * err
     return np.sqrt(answer) if normalize else answer
+
+def per_query_error2(W, A, eps=np.sqrt(2), delta=0, normalize=False):
+    """
+    modification version of per_query_error, removed the var/delta part and get direct diag()
+    so the math behid per_query_error is that W @ AtA1 @ W.T == W@A1@A1.T@W.T 
+    At current situation pcost is 1 
+    """
+    W, A = convert_implicit(W), convert_implicit(A)
+    if isinstance(W, VStack):
+        return np.concatenate([per_query_error2(Q, A, eps, delta, normalize) for Q in W.matrices])
+    delta = A.sensitivity()
+    var = 2.0/eps**2
+    AtA1 = A.gram().pinv()
+    X = W @ AtA1 @ W.T
+    err = X.diag()
+   
+    return err
 
 def per_query_error_sampling(W, A, number=100000, eps=np.sqrt(2), normalize=False):
     # note: this only works for Kronecker or explicit strategy
@@ -112,6 +129,68 @@ def per_query_error_sampling(W, A, number=100000, eps=np.sqrt(2), normalize=Fals
         ans = np.random.choice(per_query_error(W, A), number)
         delta = A.sensitivity()
     ans *= 2.0/eps**2
+    return np.sqrt(ans) if normalize else ans
+
+
+def per_query_error_sampling2(W, A, number=100000, eps=np.sqrt(2), normalize=False):
+    """
+    Computes the possible maxium diag of a workload W over opt solution as A,
+    Under current codition we dont need eps and normalize.
+    The number served as sampling draw if they are make in the situation of W is not 
+    made of Marginals, then they will randomly selection rows from W's sub matrice and 
+    generate a "marginal version of W" W' to get appox expected_error(W', A).
+    For number=100000 times this process
+
+    If W is marginal then according to line 178, all errors are the same. So by using the 
+    expected_error, they get the trace of WtW @ AtA1 as max of diag of W.
+
+    So for testing on our small dataset of smaller [n]*5 dataset, max(per_query_error2) result == min (per_query_error_sampling2)
+    Therefore we are using this method to get appoxmation of larger dataset.
+    
+    Args:
+        W (workload.Workload): the workload to evaluate.
+        A (data.Data): opt solution of .
+        number (int): the number of samples to draw (default: 100000). used in line 186
+    
+    Returns:
+        The per-query error of the workload as an array of shape (number,).
+    """
+    # note: this only works for Kronecker or explicit strategy
+    W, A = convert_implicit(W), convert_implicit(A)
+    if isinstance(W, Weighted):
+        ans = W.weight**2 * per_query_error_sampling2(W.base, A, number)
+    #elif isinstance(W, VStack) and type(A) == VStack:
+    #    m = W.shape[0]
+    #    num = lambda Wi: int(number*Wi.shape[0]/m + 1)
+    #    samples = [per_query_error_sampling(Wi,Ai.base,num(Wi)) for Wi,Ai in zip(W.matrices,A.matrices)]
+    #    weights = [Ai.weight for Ai in A.matrices]
+    #    ans = np.concatenate([err/w**2 for w, err in zip(weights, samples)])
+    elif isinstance(W, VStack):
+        m = W.shape[0]
+        num = lambda Wi: int(number*Wi.shape[0]/m + 1)
+        samples = [per_query_error_sampling2(Wi, A, num(Wi)) for Wi in W.matrices]
+        ans = np.concatenate(samples)
+    elif isinstance(W, Kronecker) and isinstance(A, Kronecker):
+        assert isinstance(A, Kronecker)
+        pieces=[per_query_error_sampling2(Wi, Ai, number) for Wi,Ai in zip(W.matrices,A.matrices)]
+        ans = np.prod(pieces, axis=0)
+    elif isinstance(W, Kronecker) and isinstance(A, workload.Marginals):
+        # optimization: if W is Marginals, all errors are the same
+        if all( type(Wi) in [workload.Identity, workload.Ones] for Wi in W.matrices ):
+            err = expected_error(W, A)
+            ans = np.repeat(err, number)
+        else:
+            # will be very slow, uses for loop
+            AtA1 = A.gram().pinv()
+            ans = np.zeros(number)
+            for i in range(number):
+                idx = [np.random.randint(Wi.shape[0]) for Wi in W.matrices]
+                w = Kronecker([Wi[j] for Wi, j in zip(W.matrices, idx)])
+                ans[i] = expected_error(w, A)
+    else:
+        ans = np.random.choice(per_query_error2(W, A), number)
+        delta = A.sensitivity()
+    #ans *= 2.0/eps**2
     return np.sqrt(ans) if normalize else ans
 
 def strategy_supports_workload(W, A):
